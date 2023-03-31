729aa53aacb4a45f7a4c103375b9934fc973e8be
Amitlpu-840 <77196415+Amitlpu-840@users.noreply.github.com>
2023-03-31 11:35:58 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
+// import express from 'express'
+
+// const app = express()
+// const PORT = 4000
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `)
+// })
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
+
+// app.get('/about', (req, res) => {
+//   res.send('This is my about route..... ')
+// })
+// module.exports = app
+
 import express from 'express'
+const app = express();
+const port = 3000;
 
-const app = express()
-const PORT = 4000
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+app.post('/Api', (req, res) => {
+  try {
+   
+    const inputArray = req.body.array;
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+  
+    const fullName = req.body.full_name;
+    const dob = req.body.dob;
+    const email = req.body.email;
+    const collegeRollNumber = req.body.college_roll_number;
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
 
-// Export the Express API
-module.exports = app
+    const userId = `${fullName.toLowerCase()}_${dob.split('-').join('')}`;
+
+    
+    const evenNumbers = inputArray.filter(num => num % 2 === 0);
+    const oddNumbers = inputArray.filter(num => num % 2 !== 0);
+    const alphabets = inputArray.filter(char => /[a-zA-Z]/.test(char)).map(char => char.toUpperCase());
+
+   
+    const response = {
+      user_id: userId,
+      is_success: true,
+      email_id: email,
+      college_roll_number: collegeRollNumber,
+      even_numbers: evenNumbers,
+      odd_numbers: oddNumbers,
+      alphabets: alphabets
+    };
+
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
